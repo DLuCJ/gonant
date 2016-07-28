55766d2383c4417049fb72fd02a66eeed635aef3
dlu <dizzylu@gmail.com>
2016-07-27 23:13:39 -0700

    replace unsafe.Pointer/casts with interface{}/type asserts

--- gonant/mzkloader.go
+++ gonant/mzkloader.go
@@ -5,7 +5,6 @@ import (
 	"io/ioutil"
 	"strings"
 	"strconv"
-	"unsafe"
 )
 
 const NUM_INSTS = 8
@@ -46,42 +45,46 @@ const (
 )
 
 //TODO: dissociate splitSonantOutput interpretation from array idx
-type sddata struct {
-	sdt int         //AUXDATA..SONG_DATA_COLUMNS
-	sdsize uint     //num bits representation
-}
-
 type sdtype struct {
-	ptr unsafe.Pointer
-	sdt sddata    
+	kind int      //AUXDATA..SONG_DATA_COLUMNS
+	name string   //float32, uint, uint8, etc
+	size uint     //num bits representation
 }
 
-func fillStructures(parm_arr [32]unsafe.Pointer) { //Song {
-	//var song Song
-	fmt.Println(*(*string)(parm_arr[AUXDATA]))
+//TODO: may already be taken care of with type assertions?
+//TODO: but doesn't indicate what kind of data contained in arrays
+type sddata struct {
+	ptr interface{}
+	sdt sdtype    
+}
 
-	for i:= SONG_DATA_OSC1_OCT; i < SONG_DATA_PATTERNS ; i++ {
+func fillStructures(parm_arr [32]interface{}) { //Song {
+	//var song Song
+	fmt.Println(*parm_arr[AUXDATA].(*string))
+
+	for i:= SONG_DATA_OSC1_OCT; i <= SONG_DATA_COLUMNS ; i++ {
 
 		if i == SONG_DATA_FX_FREQ {
-			fmt.Println(*(*[NUM_INSTS]float64)(parm_arr[i]))
-
+			fmt.Println(*parm_arr[i].(*[NUM_INSTS]float64))
+		} else if i == SONG_DATA_PATTERNS {
+			fmt.Println(*parm_arr[i].(*[NUM_INSTS][48]int64))
+		} else if i == SONG_DATA_COLUMNS {
+			fmt.Println(*parm_arr[i].(*[NUM_INSTS * 10][32]int64))
 		} else {
-			fmt.Println(*(*[NUM_INSTS]int64)(parm_arr[i]))
+			fmt.Println(*parm_arr[i].(*[NUM_INSTS]int64))
 		}
 	}
 
 	fmt.Println()
-	fmt.Println(*(*string)(parm_arr[SONG_DATA_PATTERNS]))
-	fmt.Println()
-	fmt.Println(*(*string)(parm_arr[SONG_DATA_COLUMNS]))
 
 	fmt.Println("*****")
-	fmt.Println(*(*[NUM_INSTS]int64)(parm_arr[SONG_DATA_OSC2_VOL]))
+	fmt.Println(*parm_arr[SONG_DATA_OSC2_VOL].(*[NUM_INSTS]int64))
 }
 
-func splitSonantOutput(songdata string) ([32]unsafe.Pointer){
+/* TODO: could use some big time cleaning up.. */
+func splitSonantOutput(songdata string) ([32]interface{}){
 
-	var parm_arr [32]unsafe.Pointer
+	var parm_arr [32]interface{}
 	var err error
 	var symbols []string = strings.Split(songdata, "song_data_")
 	var sdarr [32]string	
@@ -90,28 +93,74 @@ func splitSonantOutput(songdata string) ([32]unsafe.Pointer){
 		sdarr[i] = elt
 	}
 
-	parm_arr[AUXDATA] = unsafe.Pointer(&sdarr[AUXDATA])
-	parm_arr[SONG_DATA_PATTERNS] = unsafe.Pointer(&sdarr[SONG_DATA_PATTERNS])
-	parm_arr[SONG_DATA_COLUMNS] = unsafe.Pointer(&sdarr[SONG_DATA_COLUMNS])
+	parm_arr[AUXDATA] = &sdarr[AUXDATA]
 
-	for j := SONG_DATA_OSC1_OCT; j < SONG_DATA_PATTERNS; j++ {
+	for j := SONG_DATA_OSC1_OCT; j <= SONG_DATA_COLUMNS; j++ {
 		
 		var digarr [NUM_INSTS]int64
-		var parm_loc unsafe.Pointer
+		var parm_loc interface{}
 
 		tokens := strings.Split(sdarr[j], " ")
 		trimmedtok := strings.TrimSpace(tokens[1])
 		digits := strings.Split(trimmedtok, ",")
-
 		
-		if j == SONG_DATA_FX_FREQ {
+		if j == SONG_DATA_PATTERNS {
+			var sdpatarr [NUM_INSTS][48]int64
+			loctokens := strings.Split(sdarr[j], "db")
+			
+			i := 0
+			for idx, elt := range loctokens {
+				if idx == 0 {
+					continue
+				}
+				
+				var locdigarr [48]int64
+				loctrimmedtok := strings.TrimSpace(elt)
+				digits := strings.Split(loctrimmedtok, ",")
+
+				for digidx, digelt := range digits {
+					locdigarr[digidx], err = strconv.ParseInt(digelt, 10, 8)
+				}
+
+				sdpatarr[i] = locdigarr
+				i++
+			}
+			
+			parm_loc = &sdpatarr
+
+		} else if j == SONG_DATA_COLUMNS {
+
+			var sdcolarr [NUM_INSTS * 10][32]int64
+			loctokens := strings.Split(sdarr[j], "db")
+			
+			i := 0
+			for idx, elt := range loctokens {
+				if idx == 0 {
+					continue
+				}
+				
+				var locdigarr [32]int64
+				loctrimmedtok := strings.TrimSpace(elt)
+				digits := strings.Split(loctrimmedtok, ",")
+
+				for digidx, digelt := range digits {
+					locdigarr[digidx], err = strconv.ParseInt(digelt, 10, 8)
+				}
+
+				sdcolarr[i] = locdigarr
+				i++
+			}
+			
+			parm_loc = &sdcolarr
+		
+		} else if j == SONG_DATA_FX_FREQ {
 			var flotarr [NUM_INSTS]float64
 
 			for idx, elt := range digits {
 				flotarr[idx], err = strconv.ParseFloat(elt, 32)
 			}
 			
-			parm_loc = unsafe.Pointer(&flotarr)
+			parm_loc = &flotarr
 
 		} else if ((j == SONG_DATA_ENV_ATTACK) || (j == SONG_DATA_ENV_SUSTAIN) || (j == SONG_DATA_ENV_RELEASE) ) {
 			
@@ -119,14 +168,14 @@ func splitSonantOutput(songdata string) ([32]unsafe.Pointer){
 				digarr[idx], err = strconv.ParseInt(elt, 10, 32)
 			}
 
-			parm_loc = unsafe.Pointer(&digarr)
+			parm_loc = &digarr
 				
 		} else {
 			for idx, elt := range digits {
 				digarr[idx], err = strconv.ParseInt(elt, 10, 8)
 			}
 
-			parm_loc = unsafe.Pointer(&digarr)
+			parm_loc = &digarr
 		}
 
 		if err != nil {
